e1ea15c58c11def097b55a2b6c5f81690d247d65
nmhummel <natalie.hummel@gmail.com>
2020-11-19 19:56:13 -0600

    commit tree 44213fd8d1cefc311fb7b2c40b616ab26b0fc599

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+        # call the execute method on our database(db) 
+    end
+
+    def self.find(id, db)
+        # find pokemon by their id number
+        # return new pokemon object with that info
+        pokey = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        Pokemon.new(id: pokey[0], name: pokey[1], type: pokey[2], db: db)
+        # db: db --> we used the db in arguments as the value for the key db:
+    end
+
+
 end
